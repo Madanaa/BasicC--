3e9e66ea8ded01d35c669e1bb382fbaab37f671b
madanaa <madandev1994@gmail.com>
2022-10-31 23:51:22 +0530

    added

---
+++ pattern/pyramidNumber.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+ void int printNum(int n, int var) {
+    for(int i = 1; i < = n + 1; i++) {
+        cout<<i;
+    }
+    cout<<endl;
+ }
+    cout<<endl;
+    int main() {
+       int n = 1;
+       int var = 0;
+       printNum(n,var);
+       n = n + var;
+        return 0;
+    }
+
